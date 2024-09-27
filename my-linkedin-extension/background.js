4a63fbb15428d7ea1a7bc04c09cbb3ee9af009f7
Mahendra <mahendrakumar27697@gmail.com>
2024-09-27 12:01:18 +0530

    manifest file error resolved

--- my-linkedin-extension/background.js
+++ my-linkedin-extension/background.js
@@ -1,10 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url && tab.url.includes("linkedin.com/messaging")) {
         const url = new URL(tab.url);
-
         const urlParams = new URLSearchParams(url.search);
 
-        console.log(URLSearchParams);
+        console.log("URL Params:", urlParams);
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
             chatId: urlParams.get("c"),
--- my-linkedin-extension/content.js
+++ my-linkedin-extension/content.js
@@ -1,12 +1,17 @@
-(()=>{
+(() => {
     let linkedinChatGenerator;
     let currentChat = "";
-    
-    chrome.runtime.onMessage.addListener((obj,sender,response)=>{
-        const {type, value, chatId} = obj;
-        if(type === "NEW"){
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, chatId } = obj;
+        if (type === "NEW") {
             currentChat = chatId;
+            console.log("New Chat Loaded:", chatId);
             newChatLoaded();
         }
-    })
+    });
+
+    function newChatLoaded() {
+        console.log("Chat loaded, icon should be displayed.");
+    }
 })();
